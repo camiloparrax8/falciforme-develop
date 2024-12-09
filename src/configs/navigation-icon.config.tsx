a35a35d6eb7bcf8b6a5cc567c1459c79f2dd1aee
PC-HP <cparraurueta@gmail.com>
2024-12-09 16:27:14 -0500

    elimina iconos qiue no se usan en el sidebar

--- src/configs/navigation-icon.config.tsx
+++ src/configs/navigation-icon.config.tsx
@@ -1,15 +1,6 @@
 import {
     PiHouseLineDuotone,
-    PiFolderPlusFill,
     PiUserDuotone,
-    PiDnaDuotone,
-    PiCalendarDotsDuotone,
-    PiPillDuotone,
-    PiBookOpenTextDuotone,
-    PiHeartbeatDuotone,
-    PiBrowsersDuotone,
-    PiMoneyDuotone,
-    PiArrowsLeftRightFill,
     PiFolderUserBold,
 } from 'react-icons/pi'
 
@@ -18,16 +9,7 @@ export type NavigationIcons = Record<string, JSX.Element>
 const navigationIcon: NavigationIcons = {
     home: <PiHouseLineDuotone />,
     paciente: <PiUserDuotone />,
-    historiaClinica: <PiFolderPlusFill />,
-    laboratorio: <PiDnaDuotone />,
-    cita: <PiCalendarDotsDuotone />,
-    medicamento: <PiPillDuotone />,
-    sintomasCrisis: <PiHeartbeatDuotone />,
-    educate: <PiBookOpenTextDuotone />,
-    informe: <PiBrowsersDuotone />,
-    costo: <PiMoneyDuotone />,
     seguridad: <PiFolderUserBold />,
-    integracion: <PiArrowsLeftRightFill />,
 }
 
 export default navigationIcon

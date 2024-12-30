24afae152bc666972b99664cdceeb15b4af5fd5e
PC-HP <cparraurueta@gmail.com>
2024-12-30 16:58:41 -0500

    Reorganiza el modulo Examenes fisicos del formulario de la HC

---
+++ src/views/historia/mod/examenesFisicos/defaultValues.js
@@ -0,0 +1,17 @@
+const defaultValues = {
+    frecuenciaCardiaca: '',
+    frecuenciaRespiratoria: '',
+    saturacionOxigeno: '',
+    tensionArterial: '',
+    peso: '',
+    talla: '',
+    percentil: '',
+    imc: '',
+    deficitZinc: '',
+    deficitAcidoFolico: '',
+    deficitVitaminaD: '',
+    desnutricion: '',
+    obesidad: '',
+}
+
+export default defaultValues

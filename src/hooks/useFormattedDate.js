7aaa09e0ac1195f6a17e2a699ea6ecb0728bb408
PC-HP <cparraurueta@gmail.com>
2025-02-13 09:35:04 -0500

    feat (useFormattedDate): custoom hook para colocar el formato de fecha a dd/mm/aaaa

---
+++ src/hooks/useFormattedDate.js
@@ -0,0 +1,17 @@
+// src/hooks/useFormattedDate.js
+import { useMemo } from 'react';
+
+export const useFormattedDate = () => {
+  const formatDate = useMemo(() => {
+    return (isoString) => {
+      if (!isoString) return '';
+      const date = new Date(isoString);
+      const day = String(date.getDate()).padStart(2, '0');
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+    };
+  }, []);
+
+  return { formatDate };
+};

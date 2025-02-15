88698ff17be8a1273de9cfcf20032a5ec25455fb
PC-HP <cparraurueta@gmail.com>
2025-02-15 18:10:11 -0500

    feat(services): agregar servicio POST para crear paciente con asignación manual de formData

--- src/customService/services/pacienteService.js
+++ src/customService/services/pacienteService.js
@@ -24,3 +24,36 @@ export const buscarPaciente = async (token, identificacion) => {
         throw error; // Lanza el error para manejarlo desde el componente que consume este servicio
     }
 };
+
+
+export const crearPaciente = async (token, idUsuario, formData) => {
+    try {
+        const data = {
+            nombre: formData.nombre,                  // Igual al formData
+            apellido: formData.apellido,
+            tipo_identificacion: formData.tipo_identificacion,
+            identificacion: formData.identificacion,
+            celular: formData.celular,
+            correo: formData.correo,
+            municipio: formData.municipio,
+            departamento: formData.departamento,
+            direccion: formData.direccion,
+            ocupacion: formData.ocupacion,
+            tipo_vivienda: formData.tipo_vivienda,
+            nivel_ingreso: formData.nivel_ingreso,
+            nivel_academico: formData.nivel_academico,
+            tipo_vehiculo: formData.tipo_vehiculo,
+            id_user_create: idUsuario                // Parámetro recibido
+        };
+
+        const result = await axiosInstance.post(`/pacientes`, data, {
+            headers: { Authorization: token }
+        });
+
+        return result.data;
+    } catch (error) {
+        console.error("Error al crear paciente:", error.response?.data || error.message);
+        throw error;
+    }
+};
+

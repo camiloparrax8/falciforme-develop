11b1d241e789e8714cf903cade4b4d44c601cf48
PC-HP <cparraurueta@gmail.com>
2025-02-19 11:50:14 -0500

    feat (redPrimariaService) : crea el metodo actualizar red primaria del paciente

--- src/customService/services/redPrimariaService.js
+++ src/customService/services/redPrimariaService.js
@@ -1,13 +1,40 @@
-import axiosInstance from "../adapters/axiosInstance";
+import axiosInstance from '../adapters/axiosInstance'
 
 export const buscarHospital = async (token, hospital) => {
     try {
         const result = await axiosInstance.get(`/redPrimaria/${hospital}`, {
-            headers: { Authorization: token }
-        });
-        return result.data;
+            headers: { Authorization: token },
+        })
+        return result.data
     } catch (error) {
-        console.error("Error al buscar la red primaria:", error.response?.data || error.message);
-        throw error; // Lanza el error para manejarlo desde el componente que consume este servicio
+        console.error(
+            'Error al buscar la red primaria:',
+            error.response?.data || error.message,
+        )
+        throw error // Lanza el error para manejarlo desde el componente que consume este servicio
     }
-};
+}
+export const actualizarRedPrimaraPaciente = async (token, formData) => {
+  
+    
+    try {
+        const data = {
+            idPaciente: formData.idPaciente,
+            idRedPrimaria: formData.idRedPrimaria,
+        }
+        
+       
+
+        const result = await axiosInstance.put(`/paciente/redPrimaria`, data, {
+            headers: { Authorization: token },
+        })
+
+        return result.data
+    } catch (error) {
+        console.error(
+            'Error al actualizar la red primaria:',
+            error.response?.data || error.message,
+        )
+        throw error
+    }
+}

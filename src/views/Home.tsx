3ef4d136dba80971d9aade30928e0d4d93873146
Camilo <camilopuche73@gmail.com>
2025-04-08 17:30:46 -0500

    refactor: crear graficas de muestra

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,31 +1,39 @@
-import { useSessionUser } from '@/store/authStore'
-import { useState } from 'react'
-import FormAcompañante from './paciente/add/Acompañante/FormAcompañante'
+import SimpleDonut from '@/views/common/SimpleDonut'
+import SplineArea from '@/views/common/SplineArea'
+import BasicLine from '@/views/common/BasicLine'
+import DashedLine from '@/views/common/DashedLine'
 
 const Home = () => {
-    const { user,  token, expiresIn } = useSessionUser()
-    const [isOpen, setIsOpen] = useState(false)
-    const openDialog = () => setIsOpen(true)
-    const closeDialog = () => setIsOpen(false)
     return (
-        <div>
-            <h1>
-                Hola, {user.nombres} {user.apellidos}
-            </h1>
-            <p>Correo: {user.correo}</p>
-            <p>expiresIn: {expiresIn }</p>
-            <p>token: {token}</p>
-            <p>ID: {user.id}</p>
-            <button onClick={openDialog}>Agregar Acompañante</button>
-            <FormAcompañante
-                isOpen={isOpen}
-                onClose={closeDialog}
-                onRequestClose={closeDialog}
-            />
+        <div className="flex flex-col gap-4 p-4">
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-[27px]">
+                <div className="bg-white p-4 rounded-lg shadow">
+                    <h2 className="text-lg font-semibold mb-4">
+                        Estadísticas de Soportes Transfusionales
+                    </h2>
+                    <SimpleDonut />
+                </div>
+                <div className="bg-white p-4 rounded-lg shadow">
+                    <h2 className="text-lg font-semibold mb-4">
+                        Estadísticas de atenciones
+                    </h2>
+                    <SplineArea />
+                </div>
+                <div className="bg-white p-4 rounded-lg shadow">
+                    <h2 className="text-lg font-semibold mb-4">
+                        Estadísticas de atenciones por mes
+                    </h2>
+                    <BasicLine />
+                </div>
+                <div className="bg-white p-4 rounded-lg shadow">
+                    <h2 className="text-lg font-semibold mb-4">
+                        Estadísticas de Soportes Transfusionales
+                    </h2>
+                    <DashedLine />
+                </div>
+            </div>
         </div>
-
-                    
-
     )
 }
 

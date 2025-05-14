324ed5a6985dc42be43dc433af5019b49d65ca51
Camilo Puche <104383069+CamiloPuche@users.noreply.github.com>
2025-05-14 17:10:21 -0500

    refactor: actual;izar inputs de form add paciente

--- src/views/common/form/SelectDocumentType.tsx
+++ src/views/common/form/SelectDocumentType.tsx
@@ -1,45 +1,51 @@
-import React from 'react'
-import Select from '@/components/ui/Select'
-import { Controller } from 'react-hook-form'
-import Label from './Label'
+import React from "react";
+import Select from "@/components/ui/Select";
+import { Controller } from "react-hook-form";
+import Label from "./Label";
 
-const SelectDocumentType = ({ control, name, rules, errors, className  }) => {
-    // Opciones quemadas para el tipo de documento
-    const documentTypeOptions = [
-        { value: 'CC', label: 'Cédula de Ciudadanía (CC)' },
-        { value: 'TI', label: 'Tarjeta de Identidad (TI)' },
-        { value: 'PS', label: 'Pasaporte (PS)' },
-    ]
+const SelectDocumentType = ({
+  control,
+  name,
+  rules,
+  errors,
+  className,
+  disabled,
+}) => {
+  // Opciones quemadas para el tipo de documento
+  const documentTypeOptions = [
+    { value: "CC", label: "Cédula de Ciudadanía (CC)" },
+    { value: "TI", label: "Tarjeta de Identidad (TI)" },
+    { value: "PS", label: "Pasaporte (PS)" },
+  ];
 
-    return (
-        <div className={className}>
-            <Label htmlFor={name} text="Tipo de Documento" />
+  return (
+    <div className={className}>
+      <Label htmlFor={name} text="Tipo de Documento" />
 
-            <Controller
-                name={name}
-                control={control}
-                rules={rules}
-                render={({ field }) => (
-                    <Select
-                        {...field}
-                        options={documentTypeOptions}
-                        placeholder="Seleccione..."
-                        value={documentTypeOptions.find(
-                            (option) => option.value === field.value,
-                        )}
-                        onChange={(option) => field.onChange(option?.value)}
-                        className={errors[name] ? 'border-red-500' : ''}
-                    />
-                )}
-            />
-
-            {errors[name] && (
-                <p className="text-red-500 text-sm mt-1">
-                    {errors[name].message}
-                </p>
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+        render={({ field }) => (
+          <Select
+            {...field}
+            options={documentTypeOptions}
+            placeholder="Seleccione..."
+            value={documentTypeOptions.find(
+              (option) => option.value === field.value
             )}
-        </div>
-    )
-}
+            onChange={(option) => field.onChange(option?.value)}
+            className={errors[name] ? "border-red-500" : ""}
+            disabled={disabled}
+          />
+        )}
+      />
 
-export default SelectDocumentType
+      {errors[name] && (
+        <p className="text-red-500 text-sm mt-1">{errors[name].message}</p>
+      )}
+    </div>
+  );
+};
+
+export default SelectDocumentType;

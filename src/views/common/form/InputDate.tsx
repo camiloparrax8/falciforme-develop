b4f8cd3ad96ebed949c72f0333cceb80dbbd1bda
Camilo Puche <104383069+CamiloPuche@users.noreply.github.com>
2025-05-14 17:38:38 -0500

    refactor: modificar Select date picker

--- src/views/common/form/InputDate.tsx
+++ src/views/common/form/InputDate.tsx
@@ -1,34 +1,42 @@
-import { Controller } from 'react-hook-form';
-import Label from './Label';
-import DatePicker from '@/components/ui/DatePicker';
+import { Controller } from "react-hook-form";
+import Label from "./Label";
+import DatePicker from "@/components/ui/DatePicker";
 
-const InputDatePickerForm = ({ control, name, rules, errors, label, placeholder, className }) => {
-    return (
-        <div className={className}>
-            <Label htmlFor={name} text={label} />
+const InputDatePickerForm = ({
+  control,
+  name,
+  rules,
+  errors,
+  label,
+  placeholder,
+  className,
+  disabled,
+}) => {
+  return (
+    <div className={className}>
+      <Label htmlFor={name} text={label} />
 
-            <Controller
-                name={name}
-                control={control}
-                rules={rules}
-                render={({ field }) => (
-                    <DatePicker
-                        {...field}
-                        placeholder={placeholder}
-                        onChange={(date) => field.onChange(date)}
-                        className={errors[name] ? 'border-red-500' : ''}
-                        maxDate={new Date()} 
-                    />
-                )}
-            />
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+        render={({ field }) => (
+          <DatePicker
+            {...field}
+            placeholder={placeholder}
+            onChange={(date) => field.onChange(date)}
+            className={errors[name] ? "border-red-500" : ""}
+            maxDate={new Date()}
+            disabled={disabled}
+          />
+        )}
+      />
 
-            {errors[name] && (
-                <p className="text-red-500 text-sm mt-1">
-                    {errors[name].message}
-                </p>
-            )}
-        </div>
-    );
+      {errors[name] && (
+        <p className="text-red-500 text-sm mt-1">{errors[name].message}</p>
+      )}
+    </div>
+  );
 };
 
 export default InputDatePickerForm;

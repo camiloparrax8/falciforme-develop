27cadbd0883a8d109b132070ebd8a7191cc60133
Camilo <camilopuche73@gmail.com>
2025-07-19 19:11:48 -0500

    feat: actualizar servicios de paciente y red primaria

--- src/customService/services/redPrimariaService.js
+++ src/customService/services/redPrimariaService.js
@@ -15,6 +15,21 @@ export const buscarHospital = async (token, hospital) => {
     }
 }
 
+export const buscarRedPrimariaPorId = async (token, idPaciente) => {
+    try {
+        const result = await axiosInstance.get(`/paciente/red/${idPaciente}`, {
+            headers: { Authorization: token },
+        })
+        return result.data
+    } catch (error) {
+        console.error(
+            'Error al buscar la red primaria del paciente:',
+            error.response?.data || error.message,
+        )
+        throw error
+    }
+}
+
 export const crearRedPrimaria = async (token, idUsuario, formData) => {
     try {
         const data = {
@@ -40,21 +55,42 @@ export const crearRedPrimaria = async (token, idUsuario, formData) => {
 }
 
 export const actualizarRedPrimaraPaciente = async (token, formData) => {
-  
-    
     try {
-        const data = {
-            idPaciente: formData.idPaciente,
-            idRedPrimaria: formData.idRedPrimaria,
+        // Si solo tiene idPaciente e idRedPrimaria, es una asignación
+        if (formData.idPaciente && formData.idRedPrimaria && Object.keys(formData).length === 2) {
+            const data = {
+                idPaciente: formData.idPaciente,
+                idRedPrimaria: formData.idRedPrimaria,
+            }
+
+
+            const result = await axiosInstance.put(`/paciente/redPrimaria`, data, {
+                headers: { Authorization: token },
+            })
+
+            return result.data
+        } else {
+            // Si tiene más datos, es una actualización de datos de la red primaria
+            const data = {
+                idPaciente: formData.idPaciente,
+                idRedPrimaria: formData.idRedPrimaria,
+                fecha: formData.fecha,
+                hospital: formData.hospital,
+                correo: formData.correo,
+                telefono: formData.telefono,
+                telefono_urgencias: formData.telefono_urgencias,
+                municipio: formData.municipio,
+                departamento: formData.departamento,
+                id_user_update: formData.id_user_update
+            }
+
+
+            const result = await axiosInstance.put(`/paciente/redPrimaria`, data, {
+                headers: { Authorization: token },
+            })
+
+            return result.data
         }
-        
-       
-
-        const result = await axiosInstance.put(`/paciente/redPrimaria`, data, {
-            headers: { Authorization: token },
-        })
-
-        return result.data
     } catch (error) {
         console.error(
             'Error al actualizar la red primaria:',
